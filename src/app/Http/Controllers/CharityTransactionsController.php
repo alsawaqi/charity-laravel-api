4884eb2ce9007d3301ecbefec9ceb4b3902c2d07
Abdallah Al Sawaqi <abdallah@mithqal.net>
2025-11-20 22:12:32 +0400

    added  amount to profile shares table

--- src/app/Http/Controllers/CharityTransactionsController.php
+++ src/app/Http/Controllers/CharityTransactionsController.php
@@ -11,24 +11,23 @@ class CharityTransactionsController extends Controller
 
 
     public function store(Request $request)
-    {  
+    {
 
-      $charity =   CharityTransactions::create([
-                              'device_id' => 1,
-                              'commission_profile_id' => 1,
-                              'total_amount' => $request->input('amount'),
-                              'bank_response' => $request->input('receipt') ? json_encode($request->input('receipt')) : null,
-                              'bank_transaction_id' => 1,
-                              'reference' => $request->input('id'),
-                              'status' => $request->input('status'),
-                ]);
+        $charity =   CharityTransactions::create([
+            'device_id' => 1,
+            'commission_profile_id' => 1,
+            'total_amount' => $request->input('amount'),
+            'bank_response' => $request->input('receipt') ? json_encode($request->input('receipt')) : null,
+            'bank_transaction_id' => 1,
+            'reference' => $request->input('id'),
+            'status' => $request->input('status'),
+        ]);
 
 
-                return response()->json([
-                'success' => true,
-                'data'    =>$charity,
-                'message' => 'charity successfully',
-            ], 201);
-      
-    }   
+        return response()->json([
+            'success' => true,
+            'data'    => $charity,
+            'message' => 'charity successfully',
+        ], 201);
+    }
 }
